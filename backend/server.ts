b38c76274bdf504f0d51b7481c55dd810d7db305
Andres-Card-WildR <andres_cardenasd@hotmail.com>
2025-06-04 23:51:30 -0500

    Agregar SearchBar, pages/Home y conectar App.tsx

---
+++ backend/server.ts
@@ -0,0 +1,27 @@
+// backend/server.ts
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = 3001;
+
+app.use(cors()); // Permite conexiones desde tu frontend
+
+// Ruta para obtener datos de campeones
+app.get('/api/champions', async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://la2.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${process.env.RIOT_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al llamar a la API de Riot' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend en http://localhost:${PORT}`);
+});
